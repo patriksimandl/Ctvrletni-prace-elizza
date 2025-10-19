78fa18b8ae65fdbd3b4de3f6a17ba384fc976f67
Patrik Simandl <patrik.simandl@seznam.cz>
2025-10-19 20:17:12 +0200

    finish zapisyVyboru and arrow animation

--- Ctvrtletni-prace-elizza-react/src/pages/zapisyVyboru/Years.jsx
+++ Ctvrtletni-prace-elizza-react/src/pages/zapisyVyboru/Years.jsx
@@ -1,24 +1,56 @@
 import { Link } from 'react-router'
 import ArrowIcon from '../../assets/icons/arrow_right.svg'
 import './Years.css'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
-export function Years({ year }) {
+export function Years({ year,winWidth }) {
   const [isHovering,setIsHovering] = useState(false);
   
-  const changeHover = () => {
-    setIsHovering(!isHovering);
+
+  const yearElement = useRef(null);
+
+  
+  const [yearElementWidth,setYearElementWidth] = useState(null)
+
+
+ 
+  useEffect( ()=>{
+    const setWidth = () =>{
+      if (yearElement.current) {
+        setYearElementWidth(yearElement.current.clientWidth);
+      }
+    }
+    
+    setWidth();
+    
+  },[winWidth])
+
+ 
+
+
+  const changeHover = async() => {
+    
+    await setIsHovering(!isHovering);
   }
+
+ 
 
   return (
     <Link 
       onMouseEnter={changeHover}
+      
       onMouseLeave={changeHover}
+      ref={yearElement}
       className='year-link'
       to={`/zápisy výboru a turnaje/${year}`}
     >
       <span className='year-link-year'>{year}</span>
-      <img className={`year-link-arrow-image ${isHovering ? 'transform-right' : 'transform-left'}`}  src={ArrowIcon}/>
+      <img 
+        className={`year-link-arrow-image`} 
+        style={{transform: isHovering ? `translateX(${yearElementWidth - 115}px) translate(-50%,-50%)` :`translateX(0%) translate(-50%,-50%)` }}
+            
+        src={ArrowIcon}
+        />
     </Link>
   )
 }
--- Ctvrtletni-prace-elizza-react/src/pages/zapisyVyboru/ZapisyVyboru.jsx
+++ Ctvrtletni-prace-elizza-react/src/pages/zapisyVyboru/ZapisyVyboru.jsx
@@ -1,7 +1,7 @@
 import { BottomMenu } from "../../BottomMenu/BottomMenu";
 import { MainMenu } from "../../MainMenu/MainMenu";
 import { zapisy, years } from "../../data/zapisyVyboru";
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router'
 import { Years } from "./Years";
 import './ZapisyVyboru.css'
@@ -10,8 +10,18 @@ import Harimoto from '../../assets/Images/harimoto-celebration.avif'
 
 export function ZapisyVyboru() {
   const [zapisyVyboru, setZapisy] = useState(zapisy);
+  const [winWidth, setWinWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setWinWidth(width);
+    };
 
 
+    window.addEventListener("resize", handleResize);
+    return () => { window.removeEventListener("resize", handleResize) }
+  }, [])
 
 
   return (
@@ -24,10 +34,10 @@ export function ZapisyVyboru() {
         <img src={Harimoto} className='background-image' />
         <div className="green-gradiant" />
         <div className="zapisy-vyboru-link-container">
-          
+
           {years.map((year) => {
             return (
-              <Years key={year} year={year} />
+              <Years key={year} year={year} winWidth={winWidth} />
             )
           })}
         </div>
